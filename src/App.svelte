b6b35581d713094d5c115fb3170e5773e92e654a
Elise Easter Christiansen <eec@dips.no>
2021-12-13 10:51:52 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	import {Router, Route} from "svelte-routing";
+	import Launch from "./launch.svelte";
+    import Front from "./Front.svelte"
+
+
+	export let url = "";
+
+</script>
+
+<Router url={url}>
+	<Route path="/">
+		<Launch/>
+	</Route>
+	<Route path="/app">
+		<Front/>
+	</Route>
+</Router>
---
+++ src/launch.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {oauth2 as Smart} from 'fhirclient';
+	import {onMount} from 'svelte';
+
+    onMount(async () => {	
+
+		Smart.authorize({
+			clientId: 'sykepenger_dokument',
+			scope: 'launch openid fhirUser patient/*.read patient/*.write offline_access documentReference/*.read documentReference/*.write',
+			patientId: "123",
+			redirectUri: './app',			
+            iss:'https://vt-selecta-b.dips.local/DIPS-WebAPI/HL7/FHIR-R4',
+			launch:	'cdp2010025::DocumentReference/ako1006604'
+
+		});
+		
+	});
+</script>  
