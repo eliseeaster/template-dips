1cba13dba2d84799b8a853f5d186fb6abe83cbcc
Elise Easter Christiansen <eec@dips.no>
2022-01-24 15:42:52 +0100

    trying to test stores

--- spec/CallbackComponent.spec.js
+++ spec/CallbackComponent.spec.js
@@ -1,7 +1,14 @@
 import { mount, asSvelteComponent } from "./support/svelte.js";
 import CallbackComponent from "../src/CallbackComponent.svelte";
+import * as CallbackComponent2 from "../src/CallbackComponent.svelte";
+import {newMethod} from "../src/CallbackComponent.svelte";
 import { tick } from "svelte";
 import { price } from "../src/stores/price.js";
+import {
+  fetch as fetchPrice,
+  rewire$fetch,
+  restore
+} from "../src/stores/price.js";
 
 const fetchOkResponse = data =>
   Promise.resolve({ ok: true, json: () => Promise.resolve(data) });
@@ -48,4 +55,73 @@ describe(CallbackComponent.name, () => {
     await tick();
     expect(container.textContent).toContain("The price is: $123.45");
   });
+});
+
+describe(CallbackComponent.name, () => {
+  asSvelteComponent();
+
+  beforeEach(() => {
+    rewire$fetch(jest.fn());
+  });
+
+  afterEach(() => {
+    restore();
+  });
+
+  //... previous two tests ...
+
+  it("test", () => {
+    const stub = jest.fn();
+    stub();
+    expect(stub).toBeCalled();
+  })
+
+  // it("test2", () => {
+  //   rewire$fetch(jest.spyOn(window, "fetch"));
+  //   mount(CallbackComponent);
+  //   expect(fetchPrice).toHaveBeenCalledTimes(0);
+  // })
+
+  it("test3", () => {
+    //mount(CallbackComponent);
+   // rewire$fetch(jest.spyOn(CallbackComponent, "newMethod"));
+   const hei = jest.fn().mockImplementation(() => "hei2")
+   const result = newMethod();
+   expect(result).toBe("hei2")
+    
+    //expect(newMethod).toHaveBeenCalledTimes(0);
+  })
+
+    // test('spyOn .toBeCalled()', () => {
+  //   const somethingSpy = jest.spyOn(myObj, 'doSomething');
+  //   myObj.doSomething();
+  //   expect(somethingSpy).toBeCalled();
+  // });
+
+    // it("fetches prices on mount", () => {
+  //   mount(CallbackComponent);
+  //   expect(fetchPrice).toHaveBeenCalled();
+  // });
+
+  it("displays the initial price", () => {
+    price.set(99.99);
+    mount(CallbackComponent);
+    expect(container.textContent).toContain("The price is: $99.99");
+  });
+
+  it("updates when the price changes", async () => {
+    mount(CallbackComponent);
+    price.set(123.45);
+    await tick();
+    expect(container.textContent).toContain("The price is: $123.45");
+  });
+
+  // test('spyOn .toBeCalled()', () => {
+  //   const somethingSpy = jest.spyOn(myObj, 'doSomething');
+  //   myObj.doSomething();
+  //   expect(somethingSpy).toBeCalled();
+  // });
+
+
+
 });
--- src/CallbackComponent.svelte
+++ src/CallbackComponent.svelte
@@ -3,6 +3,10 @@
   import { fetch as fetchPrice, price } from "./stores/price.js";
 
   onMount(fetchPrice);
+  fetchPrice();
+  fetchPrice();
+
+   export const newMethod = () => {return "hei"};
 </script>
 
 <p>The price is: ${$price}</p>
